fa72ec4afd738a4f69d1341d5e731f468e55b135
Raul Garcia <rgarcia@innova360.biz>
2024-06-03 22:07:13 -0600

    Se generan cambios en pantache index. para numero mas grandes

--- js/script_index.js
+++ js/script_index.js
@@ -24,3 +24,45 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 15000); // 3000 milisegundos = 3 segundos
 
 });
+
+const colors = [
+    '#FFD1DC', // Rosa claro
+    '#FFA07A', // Salmon claro
+    '#FFB6C1', // Rosa claro
+    '#FFC0CB', // Rosa pastel
+    '#FFDAB9', // Durazno claro
+    '#FAEBD7', // Blanco antiguo
+    '#F0FFF0', // Menta crema
+    '#FFE4E1', // Rosado pálido
+    '#FFFAF0', // Marfil antiguo
+    '#F5F5DC', // Beige
+    '#F5DEB3', // Trigo
+    '#FAF0E6', // Lino
+    '#FFF5EE', // Papaya Whip
+    '#FFF8DC', // Maíz sedoso
+    '#F0E68C', // Amarillo claro
+    '#E6E6FA', // Lavanda
+    '#FFF0F5', // Lavanda rosa
+    '#FFE4B5', // Melocotón
+    '#FDF5E6', // Blanco antiguo
+    '#FFDEAD', // Navajo blanco
+    '#F5F5F5', // Blanco humo
+    '#F0FFFF', // Azul lavanda
+    '#F5F5F5', // Gris claro
+    '#E0FFFF', // Azul claro
+    '#F5F5F5', // Blanco humo
+    '#F5F5F5', // Gris claro
+];
+
+
+let currentIndex = 0;
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % colors.length;
+    setTimeout(changeBackgroundColor, 1000); // 1 segundos para rosa pastel
+    
+}
+
+// Inicia el cambio de color
+changeBackgroundColor();
